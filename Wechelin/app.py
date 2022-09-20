994b164e6cc297b2b519122e40543a3f04f1e147
KANG <wind_breath@naver.com>
2022-09-20 13:50:50 +0900

    header index

--- Wechelin/app.py
+++ Wechelin/app.py
@@ -3,15 +3,22 @@ import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
-
-# client = MongoClient('mongodb+srv://test:sparta@cluster0.j6ve73r.mongodb.net/Cluster0?retryWrites=true&w=majority')
-# db = client.dbsparta
+client = MongoClient('mongodb+srv://test:sparta@cluster0.wzrslas.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/michelins', methods=['GET'])
+def get_restaurant():
+    restaurant_list = list(db.michelin.find({}, {'_id': False}))
+    return jsonify({'restaurant_list': restaurant_list})
+
 
 @app.route('/test')
 def test():
--- michelin.py
+++ michelin.py
@@ -7,30 +7,29 @@ client = MongoClient('mongodb+srv://test:sparta@cluster0.wzrslas.mongodb.net/Clu
 db = client.dbsparta
 
 headers = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get(
-  'https://guide.michelin.com/kr/ko/selection/south-korea/restaurants/3-stars-michelin/2-stars-michelin/1-star-michelin',
-  headers=headers)
+    'https://guide.michelin.com/kr/ko/selection/south-korea/restaurants/3-stars-michelin/2-stars-michelin/1-star-michelin',
+    headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
 
 trs = soup.select(
-  'body > main > section.section-main.search-results.search-listing-result > div.row > div > div.row.restaurant__list-row.js-toggle-result.js-geolocation.js-restaurant__list_items > div')
+    'body > main > section.section-main.search-results.search-listing-result > div.row > div > div.row.restaurant__list-row.js-toggle-result.js-geolocation.js-restaurant__list_items > div')
 
 for tr in trs:
-  image = tr.select_one('div > div.card__menu-image > a > noscript > img')['src']
-  star = tr.select_one('div > div.card__menu-content.js-match-height-content > div > i').text
-  name = tr.select_one('div > div.card__menu-content.js-match-height-content > h3 > a').text.strip()
-  type = tr.select_one('div > div.card__menu-footer.d-flex > div.card__menu-footer--price.pl-text').text.strip()
-  # 1스타 - 'm', 2스타 - 'n', 3스타 - 'o'
-  star = ord(star) - ord('l')
-  print(star)
+    image = tr.select_one('div > div.card__menu-image > a > noscript > img')['src']
+    star = tr.select_one('div > div.card__menu-content.js-match-height-content > div > i').text
+    name = tr.select_one('div > div.card__menu-content.js-match-height-content > h3 > a').text.strip()
+    type = tr.select_one('div > div.card__menu-footer.d-flex > div.card__menu-footer--price.pl-text').text.strip()
+    # 1스타 - 'm', 2스타 - 'n', 3스타 - 'o'
+    star = (ord(star) - ord('l')) * '★'
 
-  doc = {
-    'image': image,
-    'star': star,
-    'name': name,
-    'type': type
-  }
+    doc = {
+        'image': image,
+        'star': star,
+        'name': name,
+        'type': type
+    }
 
-  db.michelin.insert_one(doc)
+    db.michelin.insert_one(doc)
