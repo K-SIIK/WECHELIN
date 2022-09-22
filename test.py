bd303e5f372cc9f205930eab655b1f3cd9ce855d
KANG <wind_breath@naver.com>
2022-09-22 14:03:23 +0900

    comment edit start

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.j6ve73r.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
-
-comment = list(db.comment.find({'reviewId':19},{'_id':False}))
+# db.comment.drop()
+comment = list(db.comment.find({'reviewId': 19}, {'_id': False}))
 
 print(comment)
